69b626be8f9beccf51e15aefaed368ecbe82e526
zhangjiaxing679 <104516427+zhangjiaxing679@users.noreply.github.com>
2024-04-01 09:57:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('PyCharm3')
-
+    print_hi('!')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
